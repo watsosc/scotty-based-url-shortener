15c16f2be0d8e3751f36209f9e0b2fa1b65f8050
watsosc <watsosc@protonmail.com>
2022-01-29 22:03:20 -0500

    Adding scotty lib

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Shortener where
 
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
+import Web.Scotty
+
 shortener :: IO ()
-shortener = 
-    putStrLn "Shortener"
+shortener =
+  scotty 3000 $
+    get "/" $
+      html $
+        renderHtml $
+          H.html $
+            H.body $
+              H.h1 "Shortener"
